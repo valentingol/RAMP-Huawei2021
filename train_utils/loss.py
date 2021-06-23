9a08772cf125f5ba7acf6eca0b192da8ca076e21
valentin.goldite <valentin.goldite@gmail.com>
2021-06-23 22:33:59 +0200

    [+] add neural net and loss in source classifier

--- train_utils/loss.py
+++ train_utils/loss.py
@@ -1,14 +1,17 @@
+import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
 class UnbalancedMSE_nn(tf.keras.metrics.Metric):
-    def __init__(self, data='source', name='unblanced_mse', **kwargs):
+    def __init__(self, data='source', factor=0.9, name='unblanced_mse', **kwargs):
         """
         Parameters
         ----------
         data : str, optional
             type of data to cut. Should be 'source' or 'target',
             by default 'source'
+        factor : float, optional
+                multiplicative factor for label 1
         name : str, optional
             name of the function, by default 'unblanced_mse'
         **kwargs:
@@ -39,15 +42,18 @@ class UnbalancedMSE_nn(tf.keras.metrics.Metric):
 
 
 class UnbalancedMSE_gb(tf.keras.metrics.Metric):
-    def __init__(self, data='source'):
+    def __init__(self, data='source', factor=9.0):
         """
         Parameters
         ----------
         data : str, optional
             type of data to cut. Should be 'source' or 'target',
             by default 'source'
+        factor : float, optional
+                multiplicative factor for label 1, by default 9.0
         """
         self.data = data
+        self.factor = factor
 
     def __call__(self, y, y_pred):
         """Compute unbalenced MSE
@@ -71,6 +77,5 @@ class UnbalancedMSE_gb(tf.keras.metrics.Metric):
             for each sample point.
         (see https://lightgbm.readthedocs.io/en/latest/pythonapi/lightgbm.LGBMClassifier.html for details)
         """
-        # $$ implement unbalenced MSE $$
         grad, hess = None, None
         return grad, hess
