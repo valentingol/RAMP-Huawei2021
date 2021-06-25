ff5c26ae3fd4d0f08d4610e1fe2b67a3b45c5447
valentin.goldite <valentin.goldite@gmail.com>
2021-06-25 21:09:43 +0200

    [+] add features extractor

--- submissions/target_rf/feature_extractor.py
+++ submissions/target_rf/feature_extractor.py
@@ -1,14 +1,60 @@
 import numpy as np
+import pandas as pd
+from sklearn import preprocessing
 
-class FeatureExtractor:
+class FeatureExtractor(object):
 
     def __init__(self):
+        self.scaler = preprocessing.StandardScaler()
+        self.method = 'linear'
         pass
 
     def transform(self, X):
-        # Deal with NaNs inplace
-        np.nan_to_num(X, copy=False)
-        # We flatten the input, originally 3D (sample, time, dim) to 
-        # 2D (sample, time * dim)
-        X = X.reshape(X.shape[0], -1)
-        return X
+
+        X_new = X.copy()
+
+        for i, user in enumerate(X):
+            # set to 0 all columns that are only filled with nan
+            user[:, np.isnan(user).all(axis=0)] = 0.0
+            # fill all nan that are inside  non-nan values using 'linear'
+            df = pd.DataFrame(user).interpolate(method=self.method,
+                                                limit=None,
+                                                inplace=False,
+                                                limit_direction=None,
+                                                limit_area=None)
+            # fill all nan ouside non-nan values by copying last non-nan values
+            df = df.ffill().bfill()
+            val = df.values
+            val = self.scaler.fit_transform(val)
+            X_new[i] = val
+        return X_new
+
+    def __call__(self, X):
+        return self.transform(self, X)
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    pd.set_option('display.float_format', lambda x: '%.5f' % x)
+    # nan_factor (could be positive or negative) greater
+    # the factor greater the proportion of nan (0 correspond of
+    # ~ 0.5 of values)
+    nan_factor = 0
+    # get random inputs with different distributions over users and features
+    X = np.random.randn(100, 500, 10) * np.random.randn(100, 1, 10) * 10 \
+        + np.random.randn(100, 1, 10) * 10
+    # fill random values with nan
+    X = np.where(np.random.randn(100, 500, 10) < nan_factor, np.nan, X)
+    # take a random user
+    i = np.random.randint(0, len(X))
+    user = X[i]
+    df = pd.DataFrame(user)
+    print('before transform:')
+    print(df.describe())
+    print('missing values:\n', np.sum(np.isnan(user), axis=0))
+    fe = FeatureExtractor()
+    X = fe.transform(X)
+    user = X[20]
+    df = pd.DataFrame(user)
+    print('\nafter transform:')
+    print(df.describe())
+    print('missing values:\n', np.sum(np.isnan(user), axis=0))
